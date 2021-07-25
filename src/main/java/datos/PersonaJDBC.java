ee0985654ae9411e99247f9d5de3c71066f17d81
Kmra <kevinmoroa@gmail.com>
2021-07-25 15:40:07 -0400

    comit final

--- src/main/java/datos/PersonaJDBC.java
+++ src/main/java/datos/PersonaJDBC.java
@@ -6,19 +6,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PersonaJDBC {
-    
+
     private static final String SQL_SELECT = "SELECT id_persona, nombre, apellido,email,telefono FROM persona";
     private static final String SQL_INSERT = "INSERT INTO persona(nombre, apellido,email,telefono) VALUES(?,?,?,?)";
     private static final String SQL_UPDATE = "UPDATE persona SET nombre=?, apellido=?,email=?,telefono=? WHERE Id_persona=?";
     private static final String SQL_DELETE = "DELETE FROM persona WHERE id_persona=?";
-    
+
     public List<Persona> select() {
         Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
         Persona persona = null;
         List<Persona> personas = new ArrayList<Persona>();
-        
+
         try {
             conn = Conexion.getConnection();
             stmt = conn.prepareStatement(SQL_SELECT);
@@ -35,7 +35,7 @@ public class PersonaJDBC {
                 persona.setApellido(apellido);
                 persona.setEmail(email);
                 persona.setTelefono(telefono);
-                
+
                 personas.add(persona);
             }
         } catch (SQLException ex) {
@@ -47,12 +47,6 @@ public class PersonaJDBC {
         }
         return personas;
     }
-<<<<<<< HEAD
-
-=======
-<<<<<<< Updated upstream
-=======
->>>>>>> v1.2.0
 
     public int insert(Persona persona) {
         Connection conn = null;
@@ -126,8 +120,5 @@ public class PersonaJDBC {
         }
         return rows;
     }
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
->>>>>>> v1.2.0
+
 }
